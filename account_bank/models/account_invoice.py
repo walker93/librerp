b08f6143751a8cae0a4df3ff68ff83883f8177d2
sergio <sergiocorato@gmail.com>
2015-12-10 01:33:21 +0100

    [FIX] pep

--- account_bank/models/account_invoice.py
+++ account_bank/models/account_invoice.py
@@ -1,24 +1,22 @@
 # -*- coding: utf-8 -*-
-##############################################################################
 #
-#    Copyright (C) 2010-2013 Associazione OpenERP Italia
-#    (<http://www.openerp-italia.org>).
+#
+#    Copyright (C) 2015 SimplERP srl (<http://www.simplerp.it>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
-
+#
 from openerp.osv import fields, orm
 
 
@@ -36,9 +34,9 @@ class account_invoice(orm.Model):
         result = super(account_invoice, self).onchange_partner_id(
             cr, uid, ids, i_type, partner_id, date_invoice, payment_term,
             bank_riba_id, company_id)
-        
+
         partner = self.pool['res.partner'].browse(
-                                            cr, uid, partner_id, context)
+            cr, uid, partner_id, context)
         if partner.bank_riba_id:
             result['value']['bank_riba_id'] = partner.bank_riba_id.id
         return result
